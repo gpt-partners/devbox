f1a96d4786ad9e3e9bed1fd1c097e966f8fc21c4
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-02-18 12:59:39 +0100

    set colors in env
    bugfix missing colors

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 # Install vim
 apt-get install -y vim
+
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
 
 # Copy .vimrc
 curl https://raw.githubusercontent.com/gpt-partners/dotfiles/main/.vimrc > ~/.vimrc;
+
+# Enable colors
+export TERM=xterm-color
