cf91474f72d778cb0151c13c92b2052e06fc6472
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-02-19 00:22:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-# Made for Debian
+###################
+# Made for Debian #
+###################
+
 # Install vim
-apt-get update;
-apt-get install -y neovim;
+apt-get update
+apt-get install -y neovim
 
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Copy .vimrc
-curl -o ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/gpt-partners/dotfiles/main/.vimrc;
+curl -o ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/gpt-partners/dotfiles/main/.vimrc
 
 # Enable colors
-echo "export TERM=xterm-color" >> ~/.zshrc;
-echo "alias v=nvim" >> ~/.zshrc;
-zsh;
-source ~/.zshrc;
+echo "export TERM=xterm-color" >> ~/.zshrc
+echo "alias v=nvim" >> ~/.zshrc
+zsh
+source ~/.zshrc
 
-# Install requirements for vim and ChatGPT plugin
-python3 -m pip install pyvim dotenv
+# Install ChatGPT
+python3 -m pip install revChatGPT
+echo "alias gpt=python3 -m revChatGPT.V1" >> ~/.zshrc
