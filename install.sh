069416978c1c35f79490c7907eaedf28aa656dfe
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-10-04 01:17:38 +0200

    fix -y

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 ###################
 
 # Install neovim
-apt update -y
+apt update
 apt install -y wget
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 tar xvfz nvim-linux64.tar.gz
@@ -15,7 +15,7 @@ echo -e "export PATH=/opt/nvim-linux64/bin:$PATH" >> ~/.zshrc
 # Install Python
 #apt install -y software-properties-common
 #add-apt-repository -y ppa:deadsnakes/ppa
-#apt -y update
+#apt update
 apt remove -y python3.10 
 DEBIAN_FRONTEND=noninteractive apt install -y python3.12 python3-pip
 pip install pynvim
@@ -26,7 +26,7 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get install nodejs -y
 npm i -g prettier neovim
 
