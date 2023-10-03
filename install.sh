4ecddc0675df066c13d3be48afbec7a4f4433b91
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-10-04 01:26:00 +0200

    fix

--- install.sh
+++ install.sh
@@ -12,9 +12,9 @@ rm nvim-linux64.tar.gz
 mv nvim-linux64 /opt
 
 # Install Python
-#apt install -y software-properties-common
-#add-apt-repository -y ppa:deadsnakes/ppa
-#apt update
+apt install -y software-properties-common
+add-apt-repository -y ppa:deadsnakes/ppa
+apt update
 apt remove -y python3.10 
 DEBIAN_FRONTEND=noninteractive apt install -y python3.12 python3-pip
 pip install pynvim
@@ -24,7 +24,7 @@ apt-get install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 apt update
 apt install nodejs -y
 npm i -g prettier neovim
@@ -38,7 +38,7 @@ curl -o ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/
 
 # Enable colors
 echo "export TERM=xterm-color" >> ~/.zshrc
-echo "alias v=nvim" >> ~/.zshrc
+echo -e "alias v=nvim" >> ~/.zshrc
 
 # Install ripgrep
 apt-get install ripgrep
