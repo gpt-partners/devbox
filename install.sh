aba71e3c74e097cc5473a09d3f67d944b2053758
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-10-04 01:15:42 +0200

    Add Node.js and Python

--- install.sh
+++ install.sh
@@ -4,10 +4,31 @@
 ###################
 
 # Install neovim
-apt-get update
-wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-apt install ./nvim-linux64.deb
-rm nvim-linux64.deb
+apt update -y
+apt install -y wget
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+tar xvfz nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+mv nvim-linux64 /opt
+echo -e "export PATH=/opt/nvim-linux64/bin:$PATH" >> ~/.zshrc
+
+# Install Python
+#apt install -y software-properties-common
+#add-apt-repository -y ppa:deadsnakes/ppa
+#apt -y update
+apt remove -y python3.10 
+DEBIAN_FRONTEND=noninteractive apt install -y python3.12 python3-pip
+pip install pynvim
+
+# Install Node.js
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update -y
+sudo apt-get install nodejs -y
+npm i -g prettier neovim
 
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -22,7 +43,6 @@ echo "alias v=nvim" >> ~/.zshrc
 
 # Install ripgrep
 apt-get install ripgrep
-
 
 # Configure .gitconfig
 echo -e "[user]\n\temail = 124867543+gpt-partners@users.noreply.github.com" >> ~/.gitconfig
