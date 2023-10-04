b3261e47b480a590b02c81dbe95a56bb31b13cce
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-10-04 02:52:30 +0200

    Automatically install Plugins

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ tar xvfz nvim-linux64.tar.gz
 rm nvim-linux64.tar.gz
 mv nvim-linux64 /opt
 ln -s /opt/nvim-linux64/bin/nvim /usr/local/bin
+nvim +PlugInstall +qall >/dev/null
 
 # Install Python
 apt install python3 python3-pip -y
