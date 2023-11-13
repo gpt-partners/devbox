3b993ba54380a72ae558245d5c11a2a85c1b766b
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-11-13 01:19:24 +0100

    fix pyenv path

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ n lts
 # Install pyenv
 DEBIAN_FRONTEND=noninteractive apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 git clone https://github.com/pyenv/pyenv ~/.pyenv
-PYENV_ROOT=~/.pyenv /opt/pyenv/bin/pyenv install 3.12
+PYENV_ROOT=~/.pyenv ~/.pyenv/bin/pyenv install 3.12
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
