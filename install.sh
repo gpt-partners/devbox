870dd5b6a511625f2037e859acae30ed7fc4d9f4
gpt-partners <124867543+gpt-partners@users.noreply.github.com>
2023-11-13 02:02:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,9 +16,9 @@ n lts
 # Install pyenv
 DEBIAN_FRONTEND=noninteractive apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 git clone https://github.com/pyenv/pyenv ~/.pyenv
-PYENV_ROOT=/root/.pyenv eval "$(/root/.pyenv/bin/pyenv init -)"
-PYENV_ROOT=/root/.pyenv /root/.pyenv/bin/pyenv install 3.12
-PYENV_ROOT=/root/.pyenv /root/.pyenv/bin/pyenv global 3.12
+eval "$(/root/.pyenv/bin/pyenv init -)"
+/root/.pyenv/bin/pyenv install 3.12
+/root/.pyenv/bin/pyenv global 3.12
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc
